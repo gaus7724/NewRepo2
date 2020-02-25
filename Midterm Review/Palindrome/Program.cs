e4c0e99a697d1e22d02eb840eb5b895ffbc6725b
Gillian Gauss <gillianlizabeth@gmail.com>
2020-02-25 12:08:13 -0600

    Palindrome... still needs work

--- Midterm Review/Palindrome/Program.cs
+++ Midterm Review/Palindrome/Program.cs
@@ -6,7 +6,34 @@ namespace Palindrome
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string userInput;
+            string userInputReversed = "";
+            int index;
+            
+
+
+
+            Console.WriteLine("Please enter a word or phrase.");
+            userInput = Console.ReadLine();
+            userInput.ToLower();
+            index = userInput.Length - 1;
+
+            while (index >=0)
+            {
+                userInputReversed += userInput[index];
+                index--;
+            }
+            userInputReversed.ToLower();
+            if (userInputReversed == userInput)
+            {
+                Console.WriteLine("Congrats! Your word " + userInput + "is a palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Your word " + userInput + " is not a palindrome. Your word reversed is " + userInputReversed);
+            }
+
+            Console.ReadKey();
         }
     }
 }
